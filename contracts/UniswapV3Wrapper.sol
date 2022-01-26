1d3b874ca25707c45affe19c7a77459cb7d070f0
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2022-01-26 16:27:37 +0400

    feat: uniswap v3 oracle

---
+++ contracts/UniswapV3Wrapper.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.4 <0.9.0;
+
+import './interfaces/IExchangeWrapper.sol';
+import './interfaces/IQuoter.sol';
+
+contract UniswapV3Wrapper is IExchangeWrapper {
+  address public immutable uniswapV3Quoter;
+
+  // We assume no fee and no restrictions on the price
+  uint24 public constant SWAP_FEE = 0;
+  uint160 public constant PRICE_LIMIT = 0;
+
+  constructor(address _uniswapV3Quoter) {
+    uniswapV3Quoter = _uniswapV3Quoter;
+  }
+
+  function getAmountOut(
+    address _tokenIn,
+    uint256 _amountIn,
+    address _tokenOut
+  ) external override returns (uint256) {
+    return IQuoter(uniswapV3Quoter).quoteExactInputSingle(_tokenIn, _tokenOut, SWAP_FEE, _amountIn, PRICE_LIMIT);
+  }
+}
---
+++ contracts/interfaces/IQuoter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.4 <0.9.0;
+pragma abicoder v2;
+
+/// @notice https://github.com/Uniswap/v3-periphery/blob/main/contracts/interfaces/IQuoter.sol
+interface IQuoter {
+  function quoteExactInputSingle(
+    address tokenIn,
+    address tokenOut,
+    uint24 fee,
+    uint256 amountIn,
+    uint160 sqrtPriceLimitX96
+  ) external returns (uint256 amountOut);
+}
