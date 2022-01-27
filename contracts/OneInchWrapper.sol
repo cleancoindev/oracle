edbb4a71150ed0f657b8c9a8e5511b650287ff8a
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2022-01-27 22:32:08 +0400

    feat: 1inch oracle

---
+++ contracts/OneInchWrapper.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.4 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './interfaces/IExchangeWrapper.sol';
+import './interfaces/IOneSplitAudit.sol';
+
+contract OneInchWrapper is IExchangeWrapper {
+  uint256 constant FLAGS = 0;
+  uint256 constant PARTS = 0;
+  address public immutable oneInchAggregator;
+
+  constructor(address _oneInchAggregator) {
+    oneInchAggregator = _oneInchAggregator;
+  }
+
+  function getAmountOut(
+    address _tokenIn,
+    uint256 _amountIn,
+    address _tokenOut
+  ) external view override returns (uint256) {
+    (uint256 amountOut, ) = IOneSplitAudit(oneInchAggregator).getExpectedReturn(IERC20(_tokenIn), IERC20(_tokenOut), _amountIn, PARTS, FLAGS);
+
+    return amountOut;
+  }
+}
---
+++ contracts/interfaces/IOneSplitAudit.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.4 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+/// @notice https://github.com/1inch/1inchProtocol/blob/master/contracts/OneSplitAudit.sol
+interface IOneSplitAudit {
+  function getExpectedReturn(
+    IERC20 fromToken,
+    IERC20 destToken,
+    uint256 amount,
+    uint256 parts,
+    uint256 flags
+  ) external view returns (uint256 returnAmount, uint256[] memory distribution);
+}
