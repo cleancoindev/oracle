7ab7e909dcb97d98bd6b27567582313cf3753119
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2022-02-06 21:48:01 +0400

    refactor: move `getQuoteAtTick` to OracleLibrary

--- contracts/UniswapV3TWAPWrapper.sol
+++ contracts/UniswapV3TWAPWrapper.sol
@@ -3,13 +3,9 @@ pragma solidity >=0.8.4 <0.9.0;
 
 import './interfaces/IUniswapV3Factory.sol';
 import './libraries/OracleLibrary.sol';
-import './libraries/TickMath.sol';
-import './libraries/FullMath.sol';
 
 contract UniswapV3TWAPWrapper {
   address public immutable uniswapV3Factory;
-
-  // We assume no fee and no restrictions on the max price
   uint24 public constant SWAP_FEE = 500;
 
   constructor(address _uniswapV3Factory) {
@@ -24,15 +20,6 @@ contract UniswapV3TWAPWrapper {
   ) external view returns (uint256 _amountOut) {
     address _pool = IUniswapV3Factory(uniswapV3Factory).getPool(_tokenIn, _tokenOut, SWAP_FEE);
     int24 _timeWeightedAverageTick = OracleLibrary.consult(_pool, _timeAgo);
-    uint160 _sqrtRatioX96 = TickMath.getSqrtRatioAtTick(_timeWeightedAverageTick);
-
-    // Calculate _amountOut with better precision if it doesn't overflow when multiplied by itself
-    if (_sqrtRatioX96 <= type(uint128).max) {
-      uint256 _ratioX192 = uint256(_sqrtRatioX96) * _sqrtRatioX96;
-      _amountOut = _tokenIn < _tokenOut ? FullMath.mulDiv(_ratioX192, _amountIn, 1 << 192) : FullMath.mulDiv(1 << 192, _amountIn, _ratioX192);
-    } else {
-      uint256 _ratioX128 = FullMath.mulDiv(_sqrtRatioX96, _sqrtRatioX96, 1 << 64);
-      _amountOut = _tokenIn < _tokenOut ? FullMath.mulDiv(_ratioX128, _amountIn, 1 << 128) : FullMath.mulDiv(1 << 128, _amountIn, _ratioX128);
-    }
+    _amountOut = OracleLibrary.getQuoteAtTick(_timeWeightedAverageTick, _amountIn, _tokenIn, _tokenOut);
   }
 }
--- contracts/libraries/OracleLibrary.sol
+++ contracts/libraries/OracleLibrary.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.8.4 <0.9.0;
 
 import '../interfaces/IUniswapV3Pool.sol';
+import '../libraries/TickMath.sol';
+import '../libraries/FullMath.sol';
 
 /// @notice https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/OracleLibrary.sol
 library OracleLibrary {
@@ -23,5 +25,29 @@ library OracleLibrary {
 
     // Always round to negative infinity
     if (tickCumulativesDelta < 0 && (tickCumulativesDelta % int256(uint256(period)) != 0)) timeWeightedAverageTick--;
+  }
+
+  /// @notice Given a tick and a token amount, calculates the amount of token received in exchange
+  /// @param tick Tick value used to calculate the quote
+  /// @param baseAmount Amount of token to be converted
+  /// @param baseToken Address of an ERC20 token contract used as the baseAmount denomination
+  /// @param quoteToken Address of an ERC20 token contract used as the quoteAmount denomination
+  /// @return quoteAmount Amount of quoteToken received for baseAmount of baseToken
+  function getQuoteAtTick(
+    int24 tick,
+    uint256 baseAmount,
+    address baseToken,
+    address quoteToken
+  ) internal pure returns (uint256 quoteAmount) {
+    uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(tick);
+
+    // Calculate quoteAmount with better precision if it doesn't overflow when multiplied by itself
+    if (sqrtRatioX96 <= type(uint128).max) {
+      uint256 ratioX192 = uint256(sqrtRatioX96) * sqrtRatioX96;
+      quoteAmount = baseToken < quoteToken ? FullMath.mulDiv(ratioX192, baseAmount, 1 << 192) : FullMath.mulDiv(1 << 192, baseAmount, ratioX192);
+    } else {
+      uint256 ratioX128 = FullMath.mulDiv(sqrtRatioX96, sqrtRatioX96, 1 << 64);
+      quoteAmount = baseToken < quoteToken ? FullMath.mulDiv(ratioX128, baseAmount, 1 << 128) : FullMath.mulDiv(1 << 128, baseAmount, ratioX128);
+    }
   }
 }
